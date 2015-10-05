b213940f8bac8ee29293cca42dda5d69543092cf
Quentin Basset <Quentin@pc34.home>
2015-10-05 15:43:22 +0200

    Fix
    Fixed code after coach review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,48 +1,53 @@
-#imports
+# imports
 import fresh_tomatoes
 import media
 
 
-#insertion of the titles, resume, image and trailer of my avorites movies using the class Movie from the file media
+# insertion of the titles, resume, image and trailer of my avorites
+# movies using the class Movie from the file media
 toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toy who fuck each other",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "A story of a boy and his toy who play \
+                        together",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # noqa
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 
 avatar = media.Movie("Avatar",
-                     "A maine on a alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "A marine on a alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # noqa
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
 
-star_wars_4 = media.Movie ("Star Wars: Episode IV - A New Hope",
-                            "Luke Skywalker to the rescue, or?",
-                            "http://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",
-                            "https://www.youtube.com/watch?v=hb8kJ7F-_1s")
+star_wars_4 = media.Movie("Star Wars: Episode IV - A New Hope",
+                          "Luke Skywalker to the rescue, or?",
+                          "http://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg",  # noqa
+                          "https://www.youtube.com/watch?v=hb8kJ7F-_1s")  # noqa
 
 
 star_wars_6 = media.Movie("Star Wars: Episode VI - Return of the Jedi",
                            "Destruction of the Death Star",
-                           "http://upload.wikimedia.org/wikipedia/en/b/b2/ReturnOfTheJediPoster1983.jpg",
+                           "http://upload.wikimedia.org/wikipedia/en/b/b2/ReturnOfTheJediPoster1983.jpg",  # noqa
                            "www.youtube.com/watch?v=5UfA_aKBGMc")
 
 
 star_wars_5 = media.Movie("Star Wars: Episode V - Empire Strikes Back",
                           "The bumpy road to Jedi mastery",
-                          "http://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg",
+                          "http://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg",  # noqa
                           "www.youtube.com/watch?v=PkEKIw0FU6Y")
 
 
 gattaca = media.Movie("Gattaca",
-                      "A genetically inferior man assumes the identity of a superior one in order to pursue his lifelong dream of space travel",
-                      "http://upload.wikimedia.org/wikipedia/en/b/bb/Gataca_Movie_Poster_B.jpg",
+                      "A genetically inferior man assumes the identity\
+                      of a superior one in order to pursue his lifelon\
+                      g dream of space travel",
+                      "http://upload.wikimedia.org/wikipedia/en/b/bb/Gataca_Movie_Poster_B.jpg",  # noqa
                       "www.youtube.com/watch?v=ZppWok6SX88")
 
 
-#set all my new favorites movies in an array
-movies = [toy_story, avatar, star_wars_4, star_wars_5, star_wars_6, gattaca]
+# set all my new favorites movies in an array
+movies = [toy_story, avatar, star_wars_4, star_wars_5, star_wars_6,
+          gattaca]
 
-#launch the function open_movies_page with the list of my favorites movies in it
+# launch the function open_movies_page with the list of my favorites
+# movies in it
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -1,16 +1,21 @@
-#this file contain the class Movie
+# this file contain the class Movie
 
-import webbrowser 
+# imports
+import webbrowser
 
 
+# set of the class Movie
 class Movie():
-    """ this class provides a way to store movies information on my website"""
+    """ this class provides a way to store movies information on my we\
+    bsite"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
